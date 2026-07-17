650ee92b543e887698bec8084cc092f1de9a4a9a
Bhatsabzar <bhatsabzar066@gmail.com>
2026-07-18 00:51:58 +0530

    initial commit

---
+++ src/main/java/com/Practice/Controller/Customer.java
@@ -0,0 +1,17 @@
+package com.Practice.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cust")
+public class Customer {
+
+
+    @GetMapping("/get")
+    public String fun() {
+        return "Welcome to Docker K8 World";
+    }
+}
